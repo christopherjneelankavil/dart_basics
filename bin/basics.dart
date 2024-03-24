e0218d07745ea2be67cbf786cfef46beb1ab11e7
christopherjneelankavil <christopherneelankavil@gmail.com>
2024-03-24 23:45:15 +0530

    edited basics.dart

--- bin/basics.dart
+++ bin/basics.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
 
 void main(){
-  print('Hello World!');
-  print('Enter 2 numbers: \n');
-  var a=stdin.readLineSync();
-  var b=stdin.readLineSync();
-  int n1=int.parse(a!);
-  int n2=int.parse(b!);
-  print('Sum: ${n1+n2}');
+  //print hello world
+  // print('Hello World!');
+
+  //reading from terminal
+
+  // print('Enter 2 numbers: \n');
+  // var a=stdin.readLineSync();
+  // var b=stdin.readLineSync();
+  // int n1=int.parse(a!);
+  // int n2=int.parse(b!);
+  // print('Sum: ${n1+n2}');
 }
