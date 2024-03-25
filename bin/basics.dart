093d37009f9af8f610dabbc7e896c8cb80212464
christopherjneelankavil <christopherneelankavil@gmail.com>
2024-03-25 05:50:15 +0530

    edited

--- bin/basics.dart
+++ bin/basics.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-void main(){
+void main() {
   //print hello world
   // print('Hello World!');
 
@@ -12,4 +12,81 @@ void main(){
   // int n1=int.parse(a!);
   // int n2=int.parse(b!);
   // print('Sum: ${n1+n2}');
+
+  //data types
+
+//   int a=10;
+//   double b=2.04;
+//   num num1=9.0;
+
+//   String name='Christopher';
+//   String name2="I'm Christopher";
+//   String address='''
+//   My name
+//   My address
+// ''';
+
+// print(name.length);
+
+//Control Statements
+
+  // int a = 10;
+  // int b = 6;
+  // if (a > b) {
+  //   print('a : $a is greater');
+  // } else if (a == b) {
+  //   print('Equal');
+  // } else {
+  //   print('b : $b is greater');
+  // }
+
+  //Loops
+
+  // for(int i=0;i<10;i++){
+  //   print(i);
+  // }
+
+  //Lists
+
+//   var list1=[1,2.09,'name'];
+
+//   List<int> intList=[1,2,3];
+
+//   print(intList.length);
+//   if(intList.contains(25)){
+//     print('Yes');
+//   }
+//   else{
+//     print('No');
+//   }
+
+//   intList.add(5);
+//   intList.removeAt(0);
+
+//   List<int> numList=[10,30,56];
+//   intList.addAll(numList);
+
+//   print(intList.join(' '));
+
+//   List<List<int>> twoDList = [
+//   [1, 2, 3],
+//   [4, 5, 6]
+// ];
+// print(twoDList[0][1]);
+
+//Set
+
+// List<int> numList=[1,1,3,3,6,7];
+// Set<int> numSet={1,1,3,3,6,7};
+
+// print(numList);
+// print(numSet);
+
+// Map
+
+  Map<String, String> map1 = {
+    'name':'Christopher',
+    'address':'My address'
+  };
+  print(map1['name']);
 }
